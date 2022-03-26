25f68f94bd65092ced3cbfa78eb261f54ebf8f5c
danieltavares1301 <daniel.tavares.1301@gmail.com>
2022-03-26 16:51:22 -0300

    cria Controllers e Routers de  Sessions e Ticket

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,8 @@
+import Controller from './Controller.js';
+
+class SessionController extends Controller {
+  constructor() {
+    super('session');
+  }
+}
+export default SessionController;
---
+++ src/controllers/TicketController.js
@@ -0,0 +1,8 @@
+import Controller from './Controller.js';
+
+class TicketController extends Controller {
+  constructor() {
+    super('ticket');
+  }
+}
+export default TicketController;
